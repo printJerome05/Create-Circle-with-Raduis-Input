90fa8ceafac2aaf7f375ed8e18eaf281e23c8f56
Jerome Marco <jerome.marco.pixel8@gmail.com>
2023-07-21 18:44:08 +0800

    first commit

---
+++ circles/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Dot
+{
+    static void Main(string[] args)
+    {
+
+        double radius;
+        double thickness = 0.4;
+        ConsoleColor BorderColor = ConsoleColor.Yellow;
+        Console.ForegroundColor = BorderColor;
+        char symbol = '*';
+
+        do
+        {
+            Console.Write("Enter radius:::: ");
+            if (!double.TryParse(Console.ReadLine(), out radius) || radius <= 0)
+            {
+                Console.WriteLine("radius have to be positive number");
+            }
+        }
+        while (radius <= 0);
+
+        Console.WriteLine();
+        double rIn = radius - thickness, rOut = radius + thickness;
+
+        for (double y = radius; y >= -radius; --y)
+        {
+            for (double x = -radius; x < rOut; x += 0.5)
+            {
+                double value = x * x + y * y;
+                if (value >= rIn * rIn && value <= rOut * rOut)
+                {
+                    Console.Write(symbol);
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+}
